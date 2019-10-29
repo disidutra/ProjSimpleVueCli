4293abe97666dc0ca5370cb46cad7b863bcca677
root <di.si.dutra@gmail.com>
2019-10-28 23:37:30 -0300

    Add component user

---
+++ src/components/user/user.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "User",
+
+  data() {},
+
+  methods: {}
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import user from '@/components/user/user';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: '/user',
+      name: 'User',
+      component: user,
     },
   ],
 });
